0371c431401c48c2f1eb7fe9754fcaa4659fc4ff
Francisco Cernadas <FranciscoCernadas@outlook.es>
2022-04-13 18:58:12 -0400

    feat: commit inicial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Proyecto_Francisco_Cernadas_PR2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
